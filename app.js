9d2634d26e2626de22492b1579e1d751a81e212f
Cameron <cameron.panagrosso@gmail.com>
2017-04-20 16:03:51 -0400

    Filled out more of the database module

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const feedparser = require('feedparser-promised');
 
 const twitter = require('./twitter-bot/twitter_bot.js');
+const db = require('./database/database.js');
 
-var fetch = (url) => {
+var fetchPodcastFeed = (url) => {
     feedparser.parse(url).then( (items) => {
         items.forEach((item) => {
             console.log(`Title: ${item.title}`);
+            console.log(`Date: ${item.pubdate}\n`);
         }, this);
     }).catch( (error) => {
         console.log(`ERROR: ${error}`);
     });
 };
 
-module.exports = {
-    fetch
-};
-
-fetch('http://feeds.codenewbie.org/cnpodcast.xml');
-fetch('https://changelog.com/podcast/feed');
+fetchPodcastFeed('http://feeds.codenewbie.org/cnpodcast.xml');
+// fetch('https://changelog.com/podcast/feed');
--- database/database.js
+++ database/database.js
@@ -10,7 +10,14 @@ var fetchPodcasts = () => {
 };
 
 var savePodcasts = (podcasts) => {
-    fs.writeFileSync('podcast-data.json', JSON.stringify(podcasts));
+    try{
+        fs.writeFileSync('podcast-data.json', JSON.stringify(podcasts));
+        console.log('Saved Podcasts');
+    }catch(e){
+        console.log('Failed to save podcasts');
+        console.log(e);
+    }
+
 };
 
 var printPodcast = (podcast) => {
@@ -19,11 +26,12 @@ var printPodcast = (podcast) => {
     console.log(`Summary: ${podcast.summary}`);
 }
 
-var addLatestPodcast = (title, pubDate, summary, link) => {
+var addLatestPodcast = (series, episodeTitle, pubDate, summary, link) => {
     var podcasts = fetchPodcasts();
 
     var podcast = {
-        title,
+        series,
+        episodeTitle,
         pubDate,
         summary,
         link
@@ -32,4 +40,51 @@ var addLatestPodcast = (title, pubDate, summary, link) => {
     // TODO: add podcast to the saved list ONLY IF it is newer than the one currently saved in the
     // text-file of podcasts
 
+    var lastPodcast = getLatestPodcast(series);
+
+    if(lastPodcast){
+        var podcastDate = Date.parse(pubDate);
+        var lastPodcastDate = Date.parse(lastPodcast.pubDate);
+        if(lastPodcastDate < podcastDate){
+            if(removePodcast(series)){
+                console.log(`Removed, ${lastPodcast.series}, ${lastPodcast.title}`);
+                podcasts.push(podcast);
+                console.log(`Added: ${podcast.series}, ${podcast.title}`);
+                savePodcasts(podcasts);
+            }else{
+                console.log('Failed to remove podcast');
+            }
+        }else{
+            console.log('No new podcasts');
+        }
+    }else{
+        console.log('New podcast series detected, adding new podcast.');
+        podcasts.push(podcast);
+        savePodcasts(podcasts);
+    }
 };
+
+var removePodcast = (series) => {
+    var podcasts = fetchPodcasts();
+    var filteredPodcasts = podcasts.filter((podcast) => podcast.series !== series);
+    savePodcasts(filteredPodcasts);
+    
+    return podcasts.length != filteredPodcasts.length;
+}
+
+var getLatestPodcast = (series) =>{
+    var podcasts = fetchPodcasts();
+
+    var latestPodcast = podcasts.filter((podcast) => {
+        return podcast.series === series;
+    });
+
+    return latestPodcast;
+}
+
+module.exports = {
+    removePodcast,
+    getLatestPodcast,
+    addLatestPodcast,
+    printPodcast
+}
