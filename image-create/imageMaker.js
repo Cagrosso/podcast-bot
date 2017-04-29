44a3c9815d418c7caebdd4a7342da9c5fd598244
Cameron <cameron.panagrosso@gmail.com>
2017-04-29 18:45:11 -0400

    started doing the photoLayout the stupid way... sigh...

--- image-create/imageMaker.js
+++ image-create/imageMaker.js
@@ -1,10 +1,12 @@
 const webshot = require('webshot');
+const fs = require('fs');
 
 module.exports.adaptForPodcast = (podcast) => {
-    
+    // Worst way to do this... sigh...
+    var adaptedTemplate = fs.createReadStream(__dirname + '/views/photoLayout.html').pipe(fs.createWriteStream(podcast.series.replace(/\s/g, '') + 'Template.html'));
 }
 
-module.exports.createPhoto = () => {
+module.exports.createPhoto = (podcast) => {
     webshot(__dirname + '/views/photoLayout.html',
         __dirname + '/photos/photo.png', 
         {
