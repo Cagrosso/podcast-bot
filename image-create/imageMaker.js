f4f1664658ebd8b5189f3f61f6bfdfe5f2530d6e
Cameron <cameron.panagrosso@gmail.com>
2017-04-28 22:20:25 -0400

    adjusted photo layout

--- image-create/imageMaker.js
+++ image-create/imageMaker.js
@@ -1,14 +1,16 @@
 const webshot = require('webshot');
 
-module.exports.adaptForPodcast
+module.exports.adaptForPodcast = (podcast) => {
+    
+}
 
 module.exports.createPhoto = () => {
     webshot(__dirname + '/views/photoLayout.html',
         __dirname + '/photos/photo.png', 
         {
             windowSize: {
-                width: 600,
-                height: 600
+                width: 730,
+                height: 360
             },
             siteType: 'file',
             defaultWhiteBackground: true,
