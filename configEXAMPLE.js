eee646f54b5f69b5d1b120c52d51717bbbf4cf06
Cameron <cameron.panagrosso@gmail.com>
2017-04-29 22:41:41 -0400

    begin implementing twitter portion

---
+++ configEXAMPLE.js
@@ -0,0 +1,15 @@
+//REMOVE THE EXAMPLE PART FOR THIS TO WORK
+
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
+}
